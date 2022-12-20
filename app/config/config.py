72a7471b2fd0599fcdf56aa3e9055d9b857c6fdc
MerlinSchaefer <48853130+MerlinSchaefer@users.noreply.github.com>
2022-12-20 07:47:56 +0100

    Rename config script add data paths

---
+++ app/config/config.py
@@ -0,0 +1,15 @@
+# config.py
+from pathlib import Path
+
+BASE_DIR = Path(__file__).parent.parent.absolute()
+CONFIG_DIR = Path(BASE_DIR, "config")
+DATA_DIR = Path(BASE_DIR, "data")
+
+# Create dirs
+DATA_DIR.mkdir(parents=True, exist_ok=True)
+
+# Assets
+PROJECTS_URL = "https://raw.githubusercontent.com/GokuMohandas/Made-With-ML/main/datasets/projects.csv"
+TAGS_URL = (
+    "https://raw.githubusercontent.com/GokuMohandas/Made-With-ML/main/datasets/tags.csv"
+)
