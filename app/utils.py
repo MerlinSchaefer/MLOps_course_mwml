c0c7a94b7eb946bcc785e3b7fe2d23581c6ade28
MerlinSchaefer <48853130+MerlinSchaefer@users.noreply.github.com>
2022-12-20 07:37:21 +0100

    Add utility functions from Modeling lesson

--- app/utils.py
+++ app/utils.py
@@ -18,6 +18,24 @@ def set_seeds(seed=13):
     random.seed(seed)
 
 
+def replace_dash(x):
+    """Replace dashes from tags and aliases"""
+    return x.replace("-", " ")
+
+
+def save_dict(d, filepath):
+    """Save dict to a json file."""
+    with open(filepath, "w") as fp:
+        json.dump(d, indent=2, sort_keys=False, fp=fp)
+
+
+def load_dict(filepath):
+    """Load a dict from a json file."""
+    with open(filepath, "r") as fp:
+        d = json.load(fp)
+    return d
+
+
 def clean_text(text, lower=True, stem=False, stopwords=STOPWORDS):
     if lower:
         text = text.lower()
