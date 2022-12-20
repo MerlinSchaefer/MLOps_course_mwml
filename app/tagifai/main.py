3aae4dd5eaac3639a8f25c476c22ca967f328d02
MerlinSchaefer <48853130+MerlinSchaefer@users.noreply.github.com>
2022-12-20 16:31:30 +0100

    Add model training to app

--- app/tagifai/main.py
+++ app/tagifai/main.py
@@ -1,8 +1,15 @@
 # tagifai/main.py
+import json
+import warnings
+from argparse import Namespace
 from pathlib import Path
+
 import pandas as pd
 import utils
 from config import config
+from tagifai import data, train
+
+warnings.filterwarnings("ignore")  # necessary for SGD max_iter warning
 
 
 def elt_data():
@@ -22,3 +29,21 @@ def elt_data():
     except NameError:
         print("No logger. Continuing without.")
         print("(Fallback logs) Data extracted and saved.")
+
+
+def train_model(args_filepath):
+    """Train a model given an arguments file"""
+    # Load labeled data
+    df = pd.read_csv(Path(config.DATA_DIR, "labeled_projects.csv"))
+
+    # Train
+    args = Namespace(**utils.load_dict(filepath=args_filepath))
+    artifacts = train.train(df=df, args=args)
+    performance = artifacts["performance"]
+    print(json.dumps(performance, indent=2))
+    print(df.head())
+
+
+if __name__ == "__main__":
+    args_filepath = Path(config.CONFIG_DIR, "args.json")
+    train_model(args_filepath)
