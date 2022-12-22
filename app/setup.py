c30dbeea5605ed8fbd1a97584c7e7584327c335f
MerlinSchaefer <48853130+MerlinSchaefer@users.noreply.github.com>
2022-12-22 10:03:59 +0100

    Add style libraries to requirements and setup

--- app/setup.py
+++ app/setup.py
@@ -7,6 +7,7 @@ with open(Path(BASE_DIR, "requirements.txt"), "r") as file:
     required_packages = [ln.strip() for ln in file.readlines()]
 
 docs_packages = ["mkdocs==1.3.0", "mkdocstrings==0.18.1"]
+style_packages = ["black==22.3.0", "flake8==3.9.2", "isort==5.10.1"]
 # setup.py
 setup(
     name="tagifai",
@@ -18,7 +19,7 @@ setup(
     python_requires=">=3.7",
     install_requires=[required_packages],
     extras_require={
-        "dev": docs_packages,
+        "dev": docs_packages + style_packages,
         "docs": docs_packages,
     },
 )
